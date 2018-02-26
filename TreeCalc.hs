441cd0dfb08e04cf82c28f9cb1dc60198d42ea33
Jan Richter <>
2018-02-26 21:34:27 +0100

    no message

--- TreeCalc.hs
+++ TreeCalc.hs
@@ -1,34 +1,45 @@
+import Prelude hiding (negate) 
 import Parser
 
+-- Functions intended for external use
+
 toNNF :: MTree -> MTree 
-toNNF = resolveNeg . resolveImpl . resolveEqu
+toNNF = negations . implications . equivalences
 
-negate' :: MTree -> MTree 
-negate' n@(Leaf _)                   = Node Negate [n]
-negate' (Node Negate [n@(Leaf _)])   = n 
-negate' (Node Negate [n@(Node _ _)]) = n
-negate' n@(Node _ _)                 = Node Negate [n] 
+toCNF :: MTree -> MTree 
+toCNF t = let nnf = toNNF t 
+          in if isCNF nnf then nnf; else toCNF . cnfStep 
 
--- TODO refactor
-resolveEqu :: MTree -> MTree 
-resolveEqu n@(Leaf _)             = n 
-resolveEqu (Node Equiv (n:m:[]))  = Node And [Node Impl [n,m]
-                                             , Node Impl [m,n]]
-resolveEqu (Node Equiv (n:nodes)) = Node And [Node Impl [n, resolveEqu $ Node Equiv nodes]
-                                             , Node Impl [resolveEqu $ Node Equiv nodes, n]]
-resolveEqu (Node op nodes)        = Node op $ map resolveEqu nodes 
+-- Helper Functions
 
--- TODO refactor
-resolveImpl :: MTree -> MTree 
-resolveImpl n@(Leaf _)        = n 
-resolveImpl (Node Impl nodes) = Node Or $ f [] nodes where 
-    f xs []     = xs
-    f xs (y:ys) = f (map negate' $ y:xs) ys 
-resolveImpl (Node op nodes)   = Node op $ map resolveImpl nodes 
+negate :: MTree -> MTree 
+negate n@(Leaf _)                   = Node Negate [n]
+negate (Node Negate [n@(Leaf _)])   = n 
+negate (Node Negate [n@(Node _ _)]) = n
+negate n@(Node _ _)                 = Node Negate [n] 
 
--- TODO refactor
-resolveNeg :: MTree -> MTree -- assumes Equivalence and Implication have been resolved
-resolveNeg (Node Negate [(Node And nodes)])     = Node Or $ map (resolveNeg . negate') nodes
-resolveNeg (Node Negate [(Node Or nodes)])      = Node And $ map (resolveNeg . negate') nodes 
-resolveNeg (Node Negate [(Node Negate [node])]) = node 
-resolveNeg node                                 = node -- ausschreiben ?
+equivalences :: MTree -> MTree 
+equivalences n@(Leaf _)             = n 
+equivalences (Node Equiv (n:m:[]))  = Node And [Node Impl [n,m], Node Impl [m,n]]
+equivalences (Node Equiv (n:nodes)) = Node And [Node Impl [n, equivalences $ Node Equiv nodes]
+                                              , Node Impl [equivalences $ Node Equiv nodes, n]]
+equivalences (Node op nodes)        = Node op $ map equivalences nodes 
+
+implications :: MTree -> MTree 
+implications n@(Leaf _)        = n 
+implications (Node Impl nodes) = Node Or $ f [] nodes where 
+                                  f xs []     = xs
+                                  f xs (y:ys) = f (map negate $ y:xs) ys 
+implications (Node op nodes)   = Node op $ map implications nodes 
+
+negations :: MTree -> MTree -- assumes Equivalence and Implication have been resolved
+negations (Node Negate [(Node And nodes)])     = Node Or $ map (negations . negate) nodes
+negations (Node Negate [(Node Or nodes)])      = Node And $ map (negations . negate) nodes 
+negations (Node Negate [(Node Negate [node])]) = node 
+negations n                                    = n 
+
+isCNF :: MTree -> Bool 
+isCNF (Leaf l) = True 
+isCNF (Node And nodes) = if 
+
+cnfStep :: MTree -> MTree
